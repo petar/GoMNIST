c6d11e747ecf40caa916db12c32a2a233e2b249f
Petar Maymounkov <petar@tumblr.com>
2013-01-07 21:44:14 +0530

    ok

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ GoMNIST
 =======
 
 GoMNIST is a Go driver for reading Yann LeCun's MNIST dataset of handwritten digits
+
+Import this packages as
+
+`import "github.com/petar/GoMNIST"`
--- mnist.go
+++ mnist.go
@@ -7,9 +7,51 @@ import (
 	"os"
 )
 
+const (
+	imageMagic = 0x00000803
+	labelMagic = 0x00000801
+)
+
+type Image []byte
+
+func ReadImageFile(name string) (rows, cols int, imgs []Image, err error) {
+	?
+}
+
+func readImageFile(r io.Reader) (rows, cols int, imgs []Image, err error) {
+	var (
+		magic int32
+		n     int32
+		nrows int32
+		ncols int32
+	)
+	if err = binary.Read(r, binary.BigEndian, &magic); err != nil {
+		return nil, err
+	}
+	if magic != imageMagic {
+		return nil, os.ErrInvalid
+	}
+	if err = binary.Read(r, binary.BigEndian, &n); err != nil {
+		return nil, err
+	}
+	if err = binary.Read(r, binary.BigEndian, &nrows); err != nil {
+		return nil, err
+	}
+	if err = binary.Read(r, binary.BigEndian, &ncols); err != nil {
+		return nil, err
+	}
+	imgs = make()
+	for i := int32(0); i < n; i++ {
+		…
+	}
+	return imgs, nil
+}
+
 type Label uint8
 
-func ReadLabelFile(name string) ([]Label, error) {
+// ReadLabelFile opens the named label file (training or test), parses it and
+// returns all labels in order.
+func ReadLabelFile(name string) (labels []Label, err error) {
 	f, err := os.Open(name)
 	if err != nil {
 		return nil, err
@@ -22,25 +64,28 @@ func ReadLabelFile(name string) ([]Label, error) {
 	return readLabelFile(z)
 }
 
-func readLabelFile(r io.Reader) ([]Label, error) {
+func readLabelFile(r io.Reader) (labels []Label, err error) {
 	var (
 		magic int32
 		n     int32
 	)
-	if err := binary.Read(r, binary.BigEndian, &magic); err != nil {
+	if err = binary.Read(r, binary.BigEndian, &magic); err != nil {
 		return nil, err
 	}
-	if err := binary.Read(r, binary.BigEndian, &n); err != nil {
+	if magic != labelMagic {
+		return nil, os.ErrInvalid
+	}
+	if err = binary.Read(r, binary.BigEndian, &n); err != nil {
 		return nil, err
 	}
 	println("Reading", n, "records")
-	ll := make([]Label, n)
+	labels = make([]Label, n)
 	for i := int32(0); i < n; i++ {
 		var l Label
 		if err := binary.Read(r, binary.BigEndian, &l); err != nil {
 			return nil, err
 		}
-		ll[int(i)] = l
+		labels[int(i)] = l
 	}
-	return ll, nil
+	return labels, nil
 }
